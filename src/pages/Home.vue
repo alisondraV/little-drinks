335df4ce5632eb065862b132e467a12860591ed5
alisondraV <alice.7414122013@gmail.com>
2025-01-03 16:33:30 -0500

    Initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    
+    <div class="card">
+      <button type="button" @click="count++">
+        Count: {{ count }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  padding: 2rem;
+  text-align: center;
+}
+
+.card {
+  margin: 2rem 0;
+}
+</style>
